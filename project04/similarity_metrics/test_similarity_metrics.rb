410d4d2aa1380dd3ba902bcf8a8a1351040d0aa2
Matt O'Neal <matthew.oneal@gmail.com>
2011-09-19 16:15:16 -0600

    Appears to be working, need to hand calculate some test cases

--- project04/similarity_metrics/test_similarity_metrics.rb
+++ project04/similarity_metrics/test_similarity_metrics.rb
@@ -1,6 +1,9 @@
-require 'similarity_metrics.rb'
+require_relative 'similarity_metrics'
 
-some1 = [0,0,0];
-some2 = [1,1,1];
+test1 = [0,1,1,0];
+test2 = [1,1,1,0];
 
-puts euclidian_distance(some1, some2)
+puts "Euclidian Distance: #{euclidian_distance(test1, test2).round(2)}"
+puts "SMC: #{SMC(test1, test2).round(2)}"
+puts "Jaccard #{jaccard(test1, test2).round(2)}"
+puts "Cosine Similarity #{cosine_similarity(test1, test2).round(2)}"
