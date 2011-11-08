e1cae4235fcfec40c81a7c1c88ae85305eacdb97
Matt O'Neal <matthew.oneal@gmail.com>
2011-11-08 05:04:12 -0700

    too tired to keep going. almost there

--- project07/ann_include.rb
+++ project07/ann_include.rb
@@ -10,6 +10,8 @@ require 'Matrix'
  This means the OO model is VERY basic.  Particularly 
    - There is no inheritance.  There are 2 node classes and 2 later classes where there should be 1 of each.
    - There is only one hidden layer and no easy way to add more.  The class methods will require major rework to use multiple hidden layers
+   - The only activation function is tanh()
+   - The train function can only have 1 training example
    - There is no error checking
    - There are very few comments
 
@@ -23,12 +25,59 @@ class ANNModel
 	
 	def initialize(num_inputs, num_outputs, hidden_layer_size) 
 		# Note that this is ONLY for a single hidden layer model.  Can make hidden an array later.
-    	@input_layer = ExternalLayer.new(num_inputs)
-    	@output_layer = ExternalLayer.new(num_outputs)
-    	@hidden_layer = HiddenLayer.new(num_inputs, num_outputs, hidden_layer_size)
+    	@input_layer = InputLayer.new(num_inputs)
+    	@output_layer = SigmoidLayer.new(hidden_layer_size, num_outputs, num_outputs)
+    	@hidden_layer = SigmoidLayer.new(num_inputs, num_outputs, hidden_layer_size)
 	end
 
-	def train_model(input_data_array, output_data_array)
+	def train_model(input_data_array, target_data_array, learning_rate, max_acceptable_error)
+		# Note that this is ONLY for a single hidden layer model with 1 training data piece
+
+		iteration = 0
+		max_iterations = 50
+
+		@hidden_layer.random_weights()
+
+		output_layer_error = Array.new(@output_layer.size)
+		output_layer_input_change_needed = Array.new(@output_layer.size)
+
+		hidden_layer_error = Array.new(@hidden_layer.size)
+		
+		current_output_values = Array.new(@output_layer.size)
+		current_output_weights = Array.new
+		output_weight_change = Array.new(@hidden_layer.size) {Array.new(@output_layer.size)}
+
+		current_output_values = self.evaluate_model(input_data_array)
+
+current_output_values.each do |value|
+puts "Iter #{iteration}: #{value}"
+end
+
+		begin 
+			# For the output layer: Find the error in output and use is to find the error in the input
+			for i in 0...@output_layer.size
+				output_layer_error[i] = target_data_array[i] - current_output_values[i]
+				output_layer_input_change_needed[i] = derivative_tanh(output_layer_error[i])
+			end
+
+			# Modify each weight based on its value
+			current_output_weights = *@hidden_layer.return_output_weights()
+			for i in 0...@hidden_layer.size
+				for j in 0...@output_layer.size
+					output_weight_change[i][j] = current_output_weights[i][j] * output_layer_error[j] * learning_rate
+				end
+			end
+
+			@hidden_layer.set_output_weights(@hidden_layer.return_output_weights()-Matrix[*output_weight_change])
+
+			iteration = iteration + 1
+
+			current_output_values = self.evaluate_model(input_data_array)
+
+current_output_values.each do |value|
+puts "Iter #{iteration}: #{value}"
+end
+		end while ((iteration < max_iterations) && (output_layer_error.max > max_acceptable_error)) # Not sure if .max works properly with negative numbers
 
 	end
 
@@ -36,7 +85,7 @@ class ANNModel
 		# Note that this is ONLY for a single hidden layer model.
 
 		# For Testing - cheese some weights
-		@hidden_layer.test_weights()
+		#@hidden_layer.test_weights()
 		# End Testing
 
 		if(input_data_array.size == @input_layer.size)
@@ -47,8 +96,9 @@ class ANNModel
 			# Calculate Values
 			@hidden_layer.calculate_output_values(@input_layer, @output_layer)
 
+			@output_layer.calculate_outputs_from_inputs()
 
-			return @output_layer.values_to_array()
+			return @output_layer.output_values_to_array()
 
 		else 
 			at_exit { puts "Error: Input array length incorrect.  Cannot evaluate ANN." }
@@ -67,9 +117,9 @@ end
 
 
 
-class HiddenLayer
+class SigmoidLayer
 	def initialize(num_inputs, num_outputs, layer_size)
-		@layer = Array.new(layer_size) {HiddenNode.new}
+		@layer = Array.new(layer_size) {SigmoidNode.new}
 		@input_weights = Matrix.build(num_inputs,layer_size)
 		@output_weights = Matrix.build(layer_size,num_outputs)
 	end
@@ -106,10 +156,33 @@ class HiddenLayer
 	end
 
 
+	def set_input_weights(new_input_weights)
+		@input_weights = new_input_weights
+	end
+
+
+	def set_output_weights(new_output_weights)
+		@output_weights = new_output_weights
+	end
+
+	def return_input_weights()
+		@input_weights
+	end
+
+	def return_output_weights()
+		@output_weights
+	end
+
 
 	def test_weights()
 		@input_weights = Matrix[ [0.1,0.2], [0.3,0.4], [0.5,0.6] ]
 		@output_weights = Matrix[ [1,2,3], [1,2,3] ]
+	end
+
+	def random_weights()
+		weight_random = Random.new()
+		@input_weights = Matrix[ [weight_random.rand(-1.0..1.0),weight_random.rand(-1.0..1.0)], [weight_random.rand(-1.0..1.0),weight_random.rand(-1.0..1.0)], [weight_random.rand(-1.0..1.0),weight_random.rand(-1.0..1.0)] ]
+		@output_weights = Matrix[ [weight_random.rand(-1.0..1.0),weight_random.rand(-1.0..1.0),weight_random.rand(-1.0..1.0)], [weight_random.rand(-1.0..1.0),weight_random.rand(-1.0..1.0),weight_random.rand(-1.0..1.0)] ]
 	end
 
 
@@ -118,7 +191,7 @@ class HiddenLayer
 		layer_input_matrix = Matrix.row_vector(input_layer.values_to_array())
 		self.set_values_from_array((layer_input_matrix*@input_weights).to_a()[0])
 		calculate_outputs_from_inputs()
-		output_layer.set_values_from_array((Matrix.row_vector(output_values_to_array())*@output_weights).to_a)
+		output_layer.set_values_from_array((Matrix.row_vector(output_values_to_array())*@output_weights).to_a[0])
 	end
 
 
@@ -136,9 +209,9 @@ end
 
 
 
-class ExternalLayer
+class InputLayer
 	def initialize(num_nodes)
-		@layer = Array.new(num_nodes) {ExternalNode.new}
+		@layer = Array.new(num_nodes) {InputNode.new}
 	end
 
 
@@ -180,7 +253,7 @@ end
 
 
 
-class HiddenNode
+class SigmoidNode
 	# All nodes for this simple example have an tanh activation function Math.tanh()
 	def initialize()
 		@input_value = 0.0
@@ -212,7 +285,7 @@ end
 
 
 
-class ExternalNode
+class InputNode
 	def initialize()
 		@node_value = 0.0
 	end
@@ -224,4 +297,13 @@ class ExternalNode
 	def return_value()
 		@node_value
 	end
+end
+
+
+
+
+
+
+def derivative_tanh(value)
+	(1-(Math.tanh(value)**2))
 end
--- project07/test_ann.rb
+++ project07/test_ann.rb
@@ -2,10 +2,12 @@ require_relative 'ann_include'
 
 tester = ANNModel.new(3,3,2)
 
-tester_results = Array.new
+tester.train_model(Array[1.0,0.25,-0.5], Array[1.0,-1.0,0.0], 0.1, 0.1)
 
-tester_results = tester.evaluate_model(Array[1,1,1])
+#tester_results = Array.new
 
-tester_results.each do |i|
-	puts i
-end
+#tester_results = tester.evaluate_model(Array[1,1,1])
+
+#tester_results.each do |i|
+#	puts i
+#end
